644e08a00db83d93299bad6d07649a4dde3d395e
thepeg <marco.pegoraro@gmail.com>
2014-03-28 17:49:53 +0100

    comment-to-code implemented
    I also did a configuration files refactoring

--- src/features/hello-world/index.js
+++ src/features/hello-world/index.js
@@ -6,4 +6,9 @@ exports.start = function() {
     prepend('body', require('./templates/title'));
     prepend('body', require('./templates/hint'));
     console.log('Feature "Hello World" complete!');
+    
+    /*D console.log('comment2code!'); */
+    
+    /* D console.log('comment2code!'); */
+    
 };
