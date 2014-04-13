7e8a02cd2a60366a54cc8cd68fae3ee5911fc3b2
thepeg <marco.pegoraro@gmail.com>
2014-04-13 10:10:15 +0200

    require full template name (with .html)
    close #1

--- src/features/hello-world/index.js
+++ src/features/hello-world/index.js
@@ -3,8 +3,8 @@ var prepend = require('dom/prepend');
 
 exports.start = function() {
     console.log('Feature "Hello World" has been started!');
-    prepend('body', require('./templates/title'));
-    prepend('body', require('./templates/hint'));
+    prepend('body', require('./templates/title.html'));
+    prepend('body', require('./templates/hint.html'));
     console.log('Feature "Hello World" complete!');
     
     /*D console.log('comment2code!'); */
