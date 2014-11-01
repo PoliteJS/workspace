e8bb00c8630f64f83c5b89e5cb71c80dcc93de7c
thepeg <marco.pegoraro@gmail.com>
2014-11-01 11:33:02 +0100

    improved inline config comments

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,20 @@
  *
  * Workspace fills the whole Webpack configuration for you, 
  * still you can override every single option for both "dev" and "prod" scenario.
+ *
  */
 
 exports.dev = function() {
-    return {};
+    return {
+        output: {
+            
+            // exports each application entry points as universal modules
+            // so the file name will be available in the global scope as it was a library
+            libraryTarget: 'umd',
+            library: ['[name]']
+            
+        }
+    };
 };
 
 exports.prod = function() {
--- workspace.conf.js
+++ workspace.conf.js
@@ -2,29 +2,49 @@
  * PoliteJS Workspace
  * ==================
  * 
- * configuration file
+ * Modify this file if you need to customise Workspace behaviour or your 
+ * folder names and structure.
+ *
+ * All the options are listed here with their default values and a
+ * detailed comment description
  *
  */
 
 module.exports = {
+    
     source: {
+        // app source folder base name
         path: 'app',
+        
+        // assets folder within your app source folder
         assets: 'assets',
-		modules: 'modules',
-		scripts: 'core',
-        styles: 'core'
+        
+        // webpack modules entry point folder within your app source folder
+        // NOTE: you can add many modules entry points folders by changing this settings to be an array
+        modules: 'modules',
+        
+        // javascript entry points folder within your app source folder
+        scripts: 'entry-points',
+        
+        // css entry points folder within your app source folder
+        styles: 'entry-points'
     },
+    
     target: {
         dev: {
             path: 'build/dev'
         }
     },
-    // subModules: [
-    //     'application',
-    //     'domain',
-    //     'ko-bindings',
-    //     'ko-components',
-    //     'utils'
-    // ],
+    
+    server: {
+        dev: {
+            port: 8080,
+            compress: true
+        }
+    },
+    
+    /**
+     * Forward specific webpack configurations
+     */
     webpack: require('./webpack.config.js'),
 };
